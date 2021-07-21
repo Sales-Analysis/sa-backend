95f7a77e9d51a19deae5bb8f4a68f3e3835d5c51
vladislav.tagayev <vladislav.tagayev@nedra.digital>
2021-07-22 00:55:16 +0300

    refacot(graph): remove example code

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,18 +5,9 @@ package graph
 
 import (
 	"context"
-	"fmt"
 	"sa-back/graph/generated"
 	"sa-back/graph/model"
 )
-
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
 
 func (r *queryResolver) ListAnalysis(ctx context.Context) ([]*model.Analysis, error) {
 	var Analyzes []*model.Analysis
@@ -30,11 +21,7 @@ func (r *queryResolver) ListAnalysis(ctx context.Context) ([]*model.Analysis, er
 	return Analyzes, nil
 }
 
-// Mutation returns generated.MutationResolver implementation.
-func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
-
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
-type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
